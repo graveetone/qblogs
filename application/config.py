d8fb12c1e6f6ce0e0ab5bd4caabe30053e06538b
graveetone <zastavskijs@gmail.com>
2023-08-23 14:56:35 +0300

    QBLOGS-8 changed db credentials

--- application/config.py
+++ application/config.py
@@ -1,7 +1,7 @@
 class Configuration():
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:skele2021ton@localhost/blog_db"
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:root@localhost/qblogs_db"
     SECRET_KEY = "svSECRET_iat"
     UPLOAD_FOLDER = "static"
     SECURITY_MSG_LOGIN = ("Вам необхідно авторизуватися!", "info")
